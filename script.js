85451546eb689838d06315840fb649f546996ada
Jaeyong Bae <jdragon.bae@gmail.com>
2025-09-11 17:07:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-function doNothing() {}
-
 function main() {
-
-var x25 = document.getElementById("x25");
-var x37 = document.getElementById("x37");
-
-try { var v31 = x25.createTBody(); } catch (e) { }
-try { var v44 = v37.getBoundingClientRect(); } catch (e) { }
-}
-
-function f0() {
-}
-function f1() {
-}
-
-function f2() {
-}
-
-function f3() {
-}
-
-function f4() {
+var x27 = document.getElementById("x27");
+try { x27.replaceWith(x49); } catch (e) { }
 }
