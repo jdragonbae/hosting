c36e5d2a2876de3682b7c0bf1b930f82c957c335
Jaeyong Bae <jdragon.bae@gmail.com>
2025-09-11 12:26:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function doNothing() {}
 
-
-function gc() {
-}
-var run_count = { "main": 0, "f0": 0, "f1": 0, "f2": 0, "f3": 0, "f4": 0, };
-
 function main() {
-var x30 = document.getElementById("x30");
-try { var v130 = document.caretRangeFromPoint(0,46); } catch (e) { }
-try { x30.close(); } catch (e) { }
-try { var v130 = document.caretRangeFromPoint(0,46); } catch (e) { }
+
+var x25 = document.getElementById("x25");
+var x37 = document.getElementById("x37");
+
+try { var v31 = x25.createTBody(); } catch (e) { }
+try { var v44 = v37.getBoundingClientRect(); } catch (e) { }
 }
 
 function f0() {
 }
 function f1() {
 }
+
 function f2() {
 }
+
 function f3() {
 }
+
 function f4() {
 }
